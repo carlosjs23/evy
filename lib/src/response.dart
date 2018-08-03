094b25bc4cad04d8cc1dbd390245630adb4ce21b
Spencer <spencer@frozor.io>
2018-08-03 15:13:36 -0400

    Response code improvements

--- lib/src/response.dart
+++ lib/src/response.dart
@@ -1,20 +1,40 @@
 import 'dart:io';
 
 class Response {
-  Response();
   HttpResponse _httpResponse;
   bool _closed = false;
   bool get closed => _closed;
 
-  void send(Object data) {
-    _httpResponse.write(data);
+  Response([HttpResponse existingResponse]) {
+    this._httpResponse = existingResponse;
+  }
+
+  /// This method will throw an exception when called if the response has already
+  /// been closed. Call this before running all methods where the request is
+  /// being modified, because the exception will bubble up.
+  /// It may be a good idea in the future to create new exception classes extending
+  /// Exception so they can be caught separately or used with `is`
+  void _checkResponseClosed() {
+    if (closed) {
+      throw new Exception('Can\'t modify request after it has already been closed');
+    }
+  }
+
+  /// Call this when the http response is done being modified, and should
+  /// be closed.
+  void _finish() {
     _httpResponse.close();
     _closed = true;
   }
 
+  void send(Object data) {
+    _checkResponseClosed();
+
+    _httpResponse.write(data);
+    _finish();
+  }
+
   static Response from(HttpResponse httpResponse) {
-    Response newResponse = Response();
-    newResponse._httpResponse = httpResponse;
-    return newResponse;
+    return Response(httpResponse);
   }
 }
