5f8286f1e6e8d55357dddd27dd683f2b7ee96f4e
Carlos A. Escobar <ingcarlosandresescobar@gmail.com>
2018-08-07 23:07:34 -0500

    Docs for router

--- lib/src/router.dart
+++ lib/src/router.dart
@@ -6,9 +6,12 @@ import 'package:evy/src/response.dart';
 
 import 'route.dart';
 
+/// The core component responsible for maintaining the middleware stack
+/// and handle the requests that comes from the app.
 class Router {
   List<Middleware> _stack = List<Middleware>();
 
+  /// Pass every request coming from the app to start processing it.
   void handle(HttpRequest httpRequest) {
     Request request = Request.from(httpRequest);
     Response response = Response(httpRequest.response);
@@ -20,6 +23,13 @@ class Router {
     _runMiddleware(0, request, response);
   }
 
+  /// Adds a [Route] to the middleware stack and returns it to allow chaining
+  /// the methods (Http verbs).
+  ///
+  /// ```
+  /// Router router = Router();
+  /// router.route('/users').get(getUsers).post(addUser);
+  /// ```
   Route route(dynamic path) {
     _checkPath(path);
     Route route = Route(path);
@@ -32,6 +42,20 @@ class Router {
     return route;
   }
 
+  /// Uses the passed middleware to process requests for the specified path
+  /// before the routes.
+  ///
+  /// ```
+  ///  router.use(path: '/greet/:name', callback: checkName);
+  ///
+  ///  void checkName(Request req, Response res, next) {
+  //      if (req.params['name'] != 'Alberto') {
+  //        res.send('Only Alberto is allowed to use this action');
+  //      } else {
+  //        next();
+  //      }
+  //   }
+  /// ```
   void use({dynamic path, Callback callback}) {
     _checkPathIsValid(path);
     Middleware middleware = Middleware(path: path, callback: callback);
@@ -54,7 +78,14 @@ class Router {
     }
   }
 
+  /// Iterates through the middleware's stack trying to match
+  /// the incoming request path with each middleware's path.
+  ///
+  /// If it match, the callback stored in the middleware will be called.
+  /// The callback it's either a middleware function or a router one.
   void _runMiddleware(int index, Request request, Response response) {
+    /// Defines the next() callback so we or a route or middleware
+    /// can call it to iterate through the middleware's stack.
     var nextCallback = () {
       _runMiddleware(++index, request, response);
     };
@@ -66,6 +97,7 @@ class Router {
     if (index >= _stack.length) {
       return finish();
     }
+
     String path = request.path;
 
     if (path == null) {
@@ -82,10 +114,12 @@ class Router {
 
     request.params = middleware.params;
 
+    /// It's a middleware so call it's callback.
     if (route == null) {
       return middleware.handleRequest(request, response, nextCallback);
     }
 
+    /// It's a route so call the route dispatch method.
     if (route != null) {
       String method = request.method;
       bool hasMethod = route.methods.contains(method);
