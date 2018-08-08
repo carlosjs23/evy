4cb052f89db30e596bf21649c84c90af7ca8d20f
Carlos A. Escobar <ingcarlosandresescobar@gmail.com>
2018-08-08 11:48:56 -0500

    Update example

--- example/main.dart
+++ example/main.dart
@@ -2,34 +2,38 @@ import 'package:evy/evy.dart';
 
 void main() {
   var app = Evy();
-  var router = Router();
 
-  app.use(router: router);
+  /// This middleware will be called only for '/greet/:name' routes
+  app.use(path: '/greet/:name', callback: checkName);
 
-  app.use(path: '*', callback: alwaysClose);
+  /// This middleware will be called only for '/greet/:name' routes.
+  /// It will be executed after checkName middleware.
+  app.use(path: '/greet/:name', callback: changeName);
 
-  router.route('/greet/:name').get(sayHello);
+  ///Routes can have a callback for process the request.
+  app.get(path: '/greet/:name', callback: sayHello);
 
-  app.listen(
-      port: 3000,
-      callback: (error) {
-        if (error != null) {
-          print(error);
-        } else {
-          print('Server listening on port 3000');
-        }
-      });
+  ///Path can be a RegExp, this route will match /evy, /evyhi, /whateverevy... .
+  app.get(path: RegExp('/.*evy'), callback: sayHello);
+
+  ///Path can be a List of Strings, this will match /users, /user and /client.
+  app.get(path: ['/users', '/user', '/client'], callback: sayHello);
+
+  app.listen(port: 3000, callback: (error) {
+    if (error != null) {
+      print(error);
+    } else {
+      print('Server listening on port 3000');
+    }
+  });
 }
 
-void alwaysClose(Request req, Response res, next) {
-  next();
-}
-
-void changeName(Request req, Response res, next) {
-  if (req.params['name'] == 'Alberto') {
-    req.params['name'] = 'Carlos';
+void sayHello(Request req, Response res, next) {
+  if (req.params['name'] != null) {
+    res.send('Hello ${req.params['name']}');
+  } else {
+    res.send('Hello');
   }
-  next();
 }
 
 void checkName(Request req, Response res, next) {
@@ -40,10 +44,9 @@ void checkName(Request req, Response res, next) {
   }
 }
 
-void sayBye(Request req, Response res, next) {
-  res.send('Bye');
-}
-
-void sayHello(Request req, Response res, next) {
+void changeName(Request req, Response res, next) {
+  if (req.params['name'] == 'Alberto') {
+    req.params['name'] = 'Carlos';
+  }
   next();
 }
